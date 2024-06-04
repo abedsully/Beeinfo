939f51f690fda2fc0a816967f0799aa573ec2f8c
abedsully <abedsully2909@gmail.com>
2024-06-04 21:31:47 +0700

    Initial Commit

---
+++ Beeinfo/BeeinfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeeinfoApp.swift
+//  Beeinfo
+//
+//  Created by Stefanus Albert Wilson on 6/4/24.
+//
+
+import SwiftUI
+
+@main
+struct BeeinfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
